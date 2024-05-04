ea95d80d29cf41bfc710ab1fa2b661a1dbfc86fe
Vidhi03082003 <97527395+Vidhi03082003@users.noreply.github.com>
2024-05-04 13:24:29 +0530

    finally

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,11 +4,11 @@ import MessageContainer from '../../components/messages/MessageContainer'
 
 const Home = () => {
   return (
-    <div className='overflow-y-auto flex sm:h-[450px] md:h-[700px]  rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30 flex-col lg:flex-row'>
+    <div className='overflow-y-auto flex   rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30 flex-col lg:flex-row'>
       <Sidebar/>
       <MessageContainer/>
     </div>
   )
 }
-
+//sm:h-[450px] md:h-[700px]
 export default Home
